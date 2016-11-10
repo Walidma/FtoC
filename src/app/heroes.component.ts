0e913bae60251004cf9df42d70f52d881f720233
Walidma <walid.mohand.amer@efrei.net>
2016-11-10 17:56:48 +0100

    Add files via upload

---
+++ src/app/heroes.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { Hero } from './hero';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
+import { OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  /*moduleId: module.id,*/
+  selector: 'my-heroes',
+  templateUrl: 'heroes.component.html',
+  styleUrls: [ 'heroes.component.css' ]
+})
+
+export class HeroesComponent implements OnInit {
+
+heroes: Hero[];
+	selectedHero: Hero;
+  title = 'Tour of Heroes';
+
+constructor(private router: Router, private heroService: HeroService) { }
+
+getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+      }
+
+  onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}
+
+ ngOnInit(): void {
+ this.getHeroes();
+  }
+
+   gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
+  }
+}
